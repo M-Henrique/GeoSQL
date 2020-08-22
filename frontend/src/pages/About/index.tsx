f19dcb89afa53b700e9f65943549efdb91e2f378
Matheus Henrique <mhenrique.silva408@gmail.com>
2020-08-22 19:12:34 -0300

    Página do esquema parcialmente finalizada

--- frontend/src/pages/About/index.tsx
+++ frontend/src/pages/About/index.tsx
@@ -14,17 +14,17 @@ export default function About() {
             <h2>Desenvolvimento</h2>
             <br />
             <p>
-               O GeoSQL+ foi desenvolvido por Guilherme Henrique e ampliado por Matheus Henrique,
-               alunos de Iniciação Científica do Departamento de Ciência da Computadação da
-               Universidade Federal de Minas Gerais. Este trabalho foi desenvolvido Sob orientação
-               do Professor Dr. Clodoveu Davis Jr. e financiado pelo PIBIC/CNPq para os próximos
-               alunos da Disciplina de Banco de Dados Geográficos e de outras disciplinas
-               correlatas.
+               O GeoSQL+ foi desenvolvido por <strong>Guilherme Henrique</strong> e ampliado por{' '}
+               <strong>Matheus Henrique</strong>, alunos de Iniciação Científica do Departamento de
+               Ciência da Computadação da Universidade Federal de Minas Gerais. Este trabalho foi
+               realizado sob a orientação do Professor Dr. Clodoveu Davis Jr. e financiado pelo
+               PIBIC/CNPq para os próximos alunos da Disciplina de Banco de Dados Geográficos e de
+               outras disciplinas correlatas.
             </p>
             <br />
 
             <p>
-               Este site é um sistema com código aberto, sobre a licença GNU v2. O propósito do
+               Este site é um sistema com código aberto, sob a licença GNU v2. O propósito do
                desenvolvimento é contar futuramente com a ação colaborativa dos demais alunos que
                utilizarão e que poderão ampliar a abrangência do sistema, criando novas
                funcionalidades, melhorando as já existentes e, principalmente, aprender durante o
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -5,12 +5,15 @@ import Landing from './pages/Landing';
 import About from './pages/About';
 import Contact from './pages/Contact';
 
+import Query from './pages/Query';
+
 function Routes() {
    return (
       <BrowserRouter>
          <Route path="/" exact component={Landing}></Route>
          <Route path="/about" exact component={About}></Route>
          <Route path="/contact" exact component={Contact}></Route>
+         <Route path="/query" exact component={Query}></Route>
       </BrowserRouter>
    );
 }
