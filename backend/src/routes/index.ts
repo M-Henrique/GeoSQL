2688c45e5eeed86bb852c055f3e4fc9aa1572891
Matheus Henrique <mhenrique.silva408@gmail.com>
2020-08-25 18:36:24 -0300

    Package-lock removido

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,5 +1,9 @@
-import { Router } from 'express';
+import { Router, response } from 'express';
 
 const routes = Router();
+
+routes.get('/', (request, response) => {
+   return response.json({ message: 'Hello' });
+});
 
 export default routes;
