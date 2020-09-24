98b0562b0ea61cdb136d4dfe0527808d2ce69e5a
Matheus Henrique <mhenrique.silva408@gmail.com>
2020-09-24 17:08:14 -0300

    Array de layer inicializa vazio agora

--- frontend/src/contexts/layers.tsx
+++ frontend/src/contexts/layers.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useRef, useState } from 'react';
 
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
@@ -20,44 +20,49 @@ export const LayersProvider: React.FC = ({ children }) => {
    const { results } = useContext(QueryContext);
    const [layers, setLayers] = useState<VectorLayer[]>([]);
 
+   const isInitialMount = useRef(true);
    useEffect(() => {
-      const resultsGeoJSON = results.map((result: any) => JSON.parse(result.geojson));
+      if (isInitialMount.current) {
+         isInitialMount.current = false;
+      } else {
+         const resultsGeoJSON = results.map((result: any) => JSON.parse(result.geojson));
 
-      const geoJSONObject = {
-         type: 'FeatureCollection',
-         features: [...resultsGeoJSON],
-      };
+         const geoJSONObject = {
+            type: 'FeatureCollection',
+            features: [...resultsGeoJSON],
+         };
 
-      const vectorSource = new VectorSource({
-         features: new GeoJSON().readFeatures(geoJSONObject, { featureProjection: 'EPSG:3857' }),
-      });
+         const vectorSource = new VectorSource({
+            features: new GeoJSON().readFeatures(geoJSONObject, { featureProjection: 'EPSG:3857' }),
+         });
 
-      const vectorLayer = new VectorLayer({
-         source: vectorSource,
-         style: new Style({
-            stroke: new Stroke({
-               color: '#678901',
-               width: 2,
-            }),
-            fill: new Fill({
-               color: '#654321',
-            }),
-            image: new RegularShape({
-               fill: new Fill({
-                  color: '#654321',
-               }),
+         const vectorLayer = new VectorLayer({
+            source: vectorSource,
+            style: new Style({
                stroke: new Stroke({
                   color: '#678901',
                   width: 2,
                }),
-               points: 4,
-               radius: 10,
-               angle: Math.PI / 4,
+               fill: new Fill({
+                  color: '#654321',
+               }),
+               image: new RegularShape({
+                  fill: new Fill({
+                     color: '#654321',
+                  }),
+                  stroke: new Stroke({
+                     color: '#678901',
+                     width: 2,
+                  }),
+                  points: 4,
+                  radius: 10,
+                  angle: Math.PI / 4,
+               }),
             }),
-         }),
-      });
+         });
 
-      setLayers((oldLayers) => [...oldLayers, vectorLayer]);
+         setLayers((oldLayers) => [...oldLayers, vectorLayer]);
+      }
    }, [results]);
 
    return <LayersContext.Provider value={{ layers }}>{children}</LayersContext.Provider>;
