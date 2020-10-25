399c3c9cc257ccbfcc4057e8d2cef84fac5fe42b
Matheus Henrique <mhenrique.silva408@gmail.com>
2020-10-25 17:11:50 -0300

    Dockerização frontend finalizada

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,6 @@ import Routes from './routes';
 import './assets/styles/globals.css';
 
 function App() {
-   console.log(process.env.REACT_APP_API_URL);
    return (
       <TablesProvider>
          <QueryProvider>
